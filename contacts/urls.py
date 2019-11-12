b617359e68ea1a8f6412b8cd9df712a78091f25b
Natalia Szokaluk-Krawczyk <nataliaszokaluk@gmail.com>
2019-11-12 21:05:51 +0100

    Changed "urls.py".

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from contacts.views import *
+from contacts.views import, ShowAll, AddNewPerson, EditPerson, DeletePerson, ShowDetails, AddAddress,
+    AddExistingAddress, DeleteAddress, AddToGroup, AddPhone, DeletePhone, AddEmail, DeleteEmail, ShowAllGroups,
+    AddNewGroup, EditGroup, DeleteGroup, ShowGroupDetails
 
 urlpatterns = [
-    path('', ShowAll.as_view(), name='show all contacts'),
+    path('', ShowAll.as_view(), name='all-contacts'),
 
-    path('new', AddNewPerson.as_view(), name='add new person'),
-    path('modify/<int:person_id>', EditPerson.as_view(), name='modify person'),
-    path('delete/<int:person_id>', DeletePerson.as_view(), name='delete person'),
-    path('show/<int:person_id>', ShowDetails.as_view(), name='show details'),
+    path('new', AddNewPerson.as_view(), name='add-new-person'),
+    path('modify/<int:person_id>', EditPerson.as_view(), name='edit-person'),
+    path('delete/<int:person_id>', DeletePerson.as_view(), name='delete-person'),
+    path('show/<int:person_id>', ShowDetails.as_view(), name='person-details'),
 
     path('modify/<int:person_id>/addAddress', AddAddress.as_view()),
     path('modify/<int:person_id>/addExistingAddress', AddExistingAddress.as_view()),
@@ -21,9 +23,9 @@ urlpatterns = [
     path('modify/<int:person_id>/addEmail', AddEmail.as_view()),
     path('modify/<int:person_id>/emailDeleted', DeleteEmail.as_view()),
 
-    path('groups', ShowAllGroups.as_view(), name='show all groups'),
-    path('new_group', AddNewGroup.as_view(), name='add new group'),
-    path('modify_group/<int:group_id>', EditGroup.as_view(), name='modify group'),
-    path('delete_group/<int:group_id>', DeleteGroup.as_view(), name='delete group'),
-    path('show_group/<int:group_id>', ShowGroupDetails.as_view(), name='group details'),
+    path('groups', ShowAllGroups.as_view(), name='all-groups'),
+    path('new_group', AddNewGroup.as_view(), name='add-new-group'),
+    path('modify_group/<int:group_id>', EditGroup.as_view(), name='edit-group'),
+    path('delete_group/<int:group_id>', DeleteGroup.as_view(), name='delete-group'),
+    path('show_group/<int:group_id>', ShowGroupDetails.as_view(), name='group-details'),
 ]
